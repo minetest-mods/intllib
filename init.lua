48d19147f7e9c0d6bccf4e5fa20db882b49aad2e
AntumDeluge <antumdeluge@gmail.com>
2017-06-20 21:33:53 -0700

    Replace deprecated 'setting_get' with 'settings:get'

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ local MP = minetest.get_modpath("intllib")
 dofile(MP.."/lib.lua")
 
 
-local LANG = minetest.setting_get("language")
+local LANG = minetest.settings:get("language")
 if not (LANG and (LANG ~= "")) then LANG = os.getenv("LANG") end
 if not (LANG and (LANG ~= "")) then LANG = "en" end
 
@@ -106,7 +106,7 @@ function intllib.get_detected_languages()
 
 	local v
 
-	v = minetest.setting_get("language")
+	v = minetest.settings:get("language")
 	if v and v~="" then
 		addlang(v)
 	end
